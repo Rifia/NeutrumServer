e7b58a2342be41cad886dfa4263743a41d0c5c6a
Marina Filina <katerain45@gmail.com>
2018-11-18 22:26:00 +0300

    finished controller

--- src/main/java/com/jaspiersin/neutrum/server/AppController.java
+++ src/main/java/com/jaspiersin/neutrum/server/AppController.java
@@ -1,16 +1,14 @@
 package com.jaspiersin.neutrum.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
-@RequestMapping(value = "/")
+@RequestMapping(value = "/incidents")
 public class AppController {
 
     @Autowired
@@ -19,17 +17,27 @@ public class AppController {
     @RequestMapping(value = "/page") //возврат страницы
     public ModelAndView index () {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("hello");
+        modelAndView.setViewName("startpage");
         return modelAndView;
     }
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public void Create(@RequestBody Incident incident){
         incidentRepository.save(incident);
     }
+
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<Incident> showAll(){
         return incidentRepository.findAll();
     }
 
+    @RequestMapping(value = "/getIncident/{id}", method = RequestMethod.GET)
+    public Optional getById (@PathVariable Long id) { //maybe this return type is a big mistake, but it works
+        return incidentRepository.findById(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public void deleteById (@PathVariable Long id) {
+        incidentRepository.deleteById(id);
+    }
 
 }
